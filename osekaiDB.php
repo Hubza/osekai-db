05ccbfed8a07162f557e109337a58416fb0c05c4
Hubz <hubsterhub@gmail.com>
2022-06-02 22:43:29 +0100

    Update osekaiDB.php

--- osekaiDB.php
+++ osekaiDB.php
@@ -1,1 +1,63 @@
+<?php
+class Database {
 
+   private static $db;
+   private $connection;
+
+   private function __construct() {
+       $this->connection = new MySQLi('localhost', 'user', 'password', 'database');
+   }
+
+   function __destruct() {
+       $this->connection->close();
+   }
+
+   public static function getConnection() {
+       if (self::$db == null) {
+           self::$db = new Database();
+       }
+       return self::$db->connection;
+   }
+
+   public static function execSelect($strQuery, $strTypes, $colVariables) {
+       $mysql = self::getConnection();
+       $stmt = $mysql->prepare($strQuery);
+       $stmt->bind_param($strTypes, ...$colVariables);
+       $stmt->execute();
+       $meta = $stmt->result_metadata(); 
+        
+       while ($field = $meta->fetch_field()) $params[] = &$row[$field->name];
+       $stmt->bind_result(...$params);       
+       while ($stmt->fetch()) {
+           foreach($row as $key => $val) {
+               $c[$key] = $val;
+            }
+            $hits[] = $c;
+        }
+        if($mysql->more_results()) {
+            $mysql->next_result();
+        }
+        if(isset($hits)) {
+            return $hits;
+        } else {
+            return [];
+        }
+        return (array)$hits;
+   }
+
+   public static function execSimpleSelect($strQuery) {
+        $oQuery = self::getConnection()->query($strQuery);
+        while ($val = $oQuery->fetch_assoc()) {
+            $hits[] = $val;
+        }
+        return $hits;
+    }
+
+   public static function execOperation($strQuery, $strTypes, $colVariables): void {
+       $mysql = self::getConnection();
+       $stmt = $mysql->prepare($strQuery);
+       $stmt->bind_param($strTypes, ...$colVariables);
+       $stmt->execute();
+   }
+
+}
