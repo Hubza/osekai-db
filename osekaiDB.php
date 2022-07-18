4760ea2dd62a70d6f76133273df5baad10fc69a0
Hubz <hubz@hubza.co.uk>
2022-07-18 12:26:09 +0100

    set charset

--- osekaiDB.php
+++ osekaiDB.php
@@ -1,63 +1,70 @@
 <?php
-class Database {
+class Database
+{
 
-   private static $db;
-   private $connection;
+    private static $db;
+    private $connection;
 
-   private function __construct() {
-       $this->connection = new MySQLi('localhost', 'user', 'password', 'database');
-   }
+    private function __construct()
+    {
+        $this->connection = new MySQLi('localhost', 'user', 'password', 'database');
+        $this->connection->set_charset("utf8mb4"); // to fix emojis in comments
+    }
 
-   function __destruct() {
-       $this->connection->close();
-   }
+    function __destruct()
+    {
+        $this->connection->close();
+    }
 
-   public static function getConnection() {
-       if (self::$db == null) {
-           self::$db = new Database();
-       }
-       return self::$db->connection;
-   }
+    public static function getConnection()
+    {
+        if (self::$db == null) {
+            self::$db = new Database();
+        }
+        return self::$db->connection;
+    }
 
-   /**
-    * @param string $strQuery
-    * @param string $strTypes
-    * @param array $colVariables
-    * 
-    * @return array
-    */
-   public static function execSelect($strQuery, $strTypes, $colVariables) {
-       $mysql = self::getConnection();
-       $stmt = $mysql->prepare($strQuery);
-       $stmt->bind_param($strTypes, ...$colVariables);
-       $stmt->execute();
-       $meta = $stmt->result_metadata(); 
-        
-       while ($field = $meta->fetch_field()) $params[] = &$row[$field->name];
-       $stmt->bind_result(...$params);       
-       while ($stmt->fetch()) {
-           foreach($row as $key => $val) {
-               $c[$key] = $val;
+    /**
+     * @param string $strQuery
+     * @param string $strTypes
+     * @param array $colVariables
+     * 
+     * @return array
+     */
+    public static function execSelect($strQuery, $strTypes, $colVariables)
+    {
+        $mysql = self::getConnection();
+        $stmt = $mysql->prepare($strQuery);
+        $stmt->bind_param($strTypes, ...$colVariables);
+        $stmt->execute();
+        $meta = $stmt->result_metadata();
+
+        while ($field = $meta->fetch_field()) $params[] = &$row[$field->name];
+        $stmt->bind_result(...$params);
+        while ($stmt->fetch()) {
+            foreach ($row as $key => $val) {
+                $c[$key] = $val;
             }
             $hits[] = $c;
         }
-        if($mysql->more_results()) {
+        if ($mysql->more_results()) {
             $mysql->next_result();
         }
-        if(isset($hits)) {
+        if (isset($hits)) {
             return $hits;
         } else {
             return [];
         }
         return (array)$hits;
-   }
+    }
 
-   /**
-    * @param string $strQuery
-    * 
-    * @return array
-    */
-   public static function execSimpleSelect($strQuery) {
+    /**
+     * @param string $strQuery
+     * 
+     * @return array
+     */
+    public static function execSimpleSelect($strQuery)
+    {
         $oQuery = self::getConnection()->query($strQuery);
         while ($val = $oQuery->fetch_assoc()) {
             $hits[] = $val;
@@ -65,18 +72,18 @@ class Database {
         return $hits;
     }
 
-   /**
-    * @param string $strQuery
-    * @param string $strTypes
-    * @param array $colVariables
-    * 
-    * @return void
-    */
-   public static function execOperation($strQuery, $strTypes, $colVariables): void {
-       $mysql = self::getConnection();
-       $stmt = $mysql->prepare($strQuery);
-       $stmt->bind_param($strTypes, ...$colVariables);
-       $stmt->execute();
-   }
-
+    /**
+     * @param string $strQuery
+     * @param string $strTypes
+     * @param array $colVariables
+     * 
+     * @return void
+     */
+    public static function execOperation($strQuery, $strTypes, $colVariables): void
+    {
+        $mysql = self::getConnection();
+        $stmt = $mysql->prepare($strQuery);
+        $stmt->bind_param($strTypes, ...$colVariables);
+        $stmt->execute();
+    }
 }
